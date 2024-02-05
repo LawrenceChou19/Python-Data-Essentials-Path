5c782dfba42d49de2243f8170406824c5bd0bd22
LawrenceChou19 <lawrencechou19@gmail.com>
2024-02-05 19:57:59 +0800

    2024-02-05

---
+++ Demo-script.py
@@ -0,0 +1,10 @@
+balance = float(input("What is your bank account blance?"))
+
+if balance < 0:
+    print("You are in debt.")
+if balance >= 0:
+    print("You are not in debt.")
+    if balance < 2000:
+        print("You cannot ask for a loan")
+    if balance >= 2000:
+        print("You can ask for a loan.")
